ea8a097968a1fab67865fc492ed847324505c889
Daria Chernobaieva <daria@douleutaras.com>
2022-10-23 16:58:48 +0300

    Initial commit

---
+++ dataverse_connecting_memory_2/main/utils.py
@@ -0,0 +1,153 @@
+from dateutil import parser
+from slugify import slugify
+
+
+def format_form_response_to_dataset(body):
+    title = body["Назва джерела"]
+    return {
+        "authority": "anAuthority",
+        "identifier": slugify(title),
+        "persistentUrl": "http://dx.doi.org/10.5072/FK2/9",
+        "protocol": "chadham-house-rule",
+        "datasetVersion": {
+            "metadataBlocks": {
+                "citation": {
+                    "displayName": "Citation Metadata",
+                    "fields": [
+                        {
+                            "typeName": "title",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": title
+                        },
+                        {
+                            "typeName": "alternativeTitle",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": body["Назва англійською"]
+                        },
+                        {
+                            "typeName": "author",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [{
+                                "authorName": {
+                                    "typeName": "authorName",
+                                    "multiple": False,
+                                    "typeClass": "primitive",
+                                    "value": body["Автор джерела"]
+                                }
+                            }]
+                        },
+                        {
+                            "typeName": "datasetContact",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [{
+                                "datasetContactEmail": {
+                                    "typeName": "datasetContactEmail",
+                                    "multiple": False,
+                                    "typeClass": "primitive",
+                                    "value": "daria.chernobaieva@gmail.com"
+                                }
+                            }]
+                        },
+                        {
+                            "typeName": "dsDescription",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [{
+                                "dsDescriptionValue": {
+                                    "typeName": "dsDescriptionValue",
+                                    "multiple": False,
+                                    "typeClass": "primitive",
+                                    "value": desc_
+                                }
+                            } for desc_ in [body["Опис українською"], body["Опис англійською (за можливості)"]]]
+                        },
+                        {
+                            "typeName": "subject",
+                            "multiple": True,
+                            "typeClass": "controlledVocabulary",
+                            "value": [
+                                "Law", "Social Sciences", "Other"
+                            ]
+                        },
+                        {
+                            "typeName": "keyword",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [{
+                                "keywordValue": {
+                                    "typeName": "keywordValue",
+                                    "multiple": False,
+                                    "typeClass": "primitive",
+                                    "value": keyword_
+                                }
+                            } for keyword_ in body["Ключові слова"].split(", ")]
+                        },
+                        {
+                            "typeName": "publication",
+                            "multiple": True,
+                            "typeClass": "compound",
+                            "value": [{
+                                "publicationURL": {
+                                    "typeName": "publicationURL",
+                                    "multiple": False,
+                                    "typeClass": "primitive",
+                                    "value": body["Інтернет посилання (для онлайн джерела)"]
+                                }
+                            }]
+                        },
+                        {
+                            "typeName": "depositor",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": body["Хто подав джерело до проєкту"]
+                        },
+                        {
+                            "typeName": "dateOfDeposit",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": parser.parse(body["Дата подання джерела до проєкту"]).strftime("%Y-%m-%d")
+                        },
+                        {
+                            "typeName": "language",
+                            "multiple": True,
+                            "typeClass": "controlledVocabulary",
+                            "value": [
+                                "Ukrainian"  # body["Мова джерела"]
+                            ]
+                        },
+                        {
+                            "typeName": "dataSources",
+                            "multiple": True,
+                            "typeClass": "primitive",
+                            "value": [
+                                body[
+                                    "Вид джерела (пост в соцмережах, стаття, новини, офіційне звернення, відеоматеріал, графічне зображення, фото, тощо)"]
+                            ]
+                        },
+                        {
+                            "typeName": "originOfSources",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": body["Звідки взято джерело (сайт, фейсбук, приватне фото, тощо)"]
+                        },
+                        {
+                            "typeName": "productionDate",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": parser.parse(body["Дата публікації джерела "]).strftime("%Y-%m-%d")
+                        },
+                        {
+                            "typeName": "productionPlace",
+                            "multiple": False,
+                            "typeClass": "primitive",
+                            "value": body["Місцевість (місцевості), згадані у джерелі"]
+                        }
+                    ]
+                }
+            }
+        }
+    }
---
+++ dataverse_connecting_memory_2/main/views.py
@@ -0,0 +1,30 @@
+import json
+
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from pyDataverse.api import NativeApi
+from pyDataverse.models import Dataset
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from main.utils import format_form_response_to_dataset
+
+
+class TypeformSubmissions(APIView):
+    permission_classes = [AllowAny]
+
+    @csrf_exempt
+    def post(self, request, *args, **kwargs):
+        body = request.data
+
+        api = NativeApi(settings.DATAVERSE_BASE_URL, settings.DATAVERSE_API_TOKEN)
+        ds = Dataset()
+        ds.from_json(json.dumps(format_form_response_to_dataset(body)))
+
+        resp = api.create_dataset(settings.DATAVERSE_PARENT_ALIAS, ds.json())
+        return Response(
+            data=resp.json(), status=status.HTTP_200_OK if resp.status_code in (200, 201)
+            else status.HTTP_400_BAD_REQUEST
+        )
